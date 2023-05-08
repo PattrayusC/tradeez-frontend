5730cb7f65e8a1bc9c935e23c3ce88a79104c6b2
Yutthasat Phoncharoenpong <pyutthasat@gmail.com>
2023-05-09 04:57:49 +0700

    Announc compo : change read more & indicator postion

--- src/components/AnnouncementPost.vue
+++ src/components/AnnouncementPost.vue
@@ -14,7 +14,7 @@
             <img src="https://down-th.img.susercontent.com/file/1dca365468f0ebedaa6b22925285cce0" class="d-block w-50 c-img" alt="Slide 1">
             <div class="carousel-caption top-0 mt-4">
               <div class="card border-0">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                   <p class="blockquote-footer mt-5 fs-6 tez-mc"> <text class="tez-mc-author ">Admin</text></p>
                   <h5 class="card-title tez-title text-truncate">ตุ๊กตาจรเข้น่ารักตะมุตะมิ</h5>
                   <h6 class="card-subtitle mb-2 tez-title2 text-truncate">พร้อมให้แลกแล้ววันนี้</h6>
@@ -27,16 +27,16 @@
           </div>
           
           <div class="carousel-item c-item">
-            <img src="https://rukminim1.flixcart.com/image/416/416/kufuikw0/stuffed-toy/m/i/y/crocodile-stuffed-plush-soft-toy-for-kids-80-cm-80-patly-original-imag7kb7waj9jzbg.jpeg?q=70" class="d-block w-50 c-img" alt="Slide 2">
+            <img src="https://rukminim1.flixcart.com/image/416/416/kufuikw0/stuffed-toy/m/i/y/crocodile-stuffed-plush-soft-toy-for-kids-80-cm-80-patly-original-imag7kb7waj9jzbg.jpeg?q=70" class="d-block w-0 c-img" alt="Slide 2">
             <div class="carousel-caption top-0 mt-4">
               <div class="card border-0" >
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                   <p class="blockquote-footer mt-5 fs-6 tez-mc"> <text class="tez-mc-author ">Admin</text></p>
                   <h5 class="card-title tez-title text-truncate">ตุ๊กตาจรเข้น่ารักตะมุตะมิ</h5>
                   <h6 class="card-subtitle mb-2 tez-title2 text-truncate">พร้อมให้แลกแล้ววันนี้</h6>
                   <p class="card-text">March 5,2023 - 4 Like</p>
                   <p class="card-text description" style="max-width: 800px;">ผู้ที่สนใจสามารถแลกได้ โดยใช้เพียง 300 คะแนน พร้อมกับค่าส่ง 100 บาท สินค้าพร้อมส่งถึงบ้านคุณ </p>
-                  <button class="btn btn-primary px-4 py-2 fs-6 tez-btn ">Read More</button>
+                  <button class="btn btn-primary px-4 py-2 fs-6 tez-btn " >Read More</button>
                 </div>
               </div>
             </div>
@@ -46,7 +46,7 @@
             <img src=" https://cdn1.takatomo.de/media/catalog/product/g/l/global-affairs-kuscheltier-krokodil-9523258.jpg" class="d-block w-50 c-img" alt="Slide 3">
             <div class="carousel-caption top-0 mt-4">
               <div class="card border-0">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                   <p class="blockquote-footer mt-5 fs-6 tez-mc"> <text class="tez-mc-author ">Admin</text></p>
                   <h5 class="card-title tez-title text-truncate">ตุ๊กตาจรเข้น่ารักตะมุตะมิ</h5>
                   <h6 class="card-subtitle mb-2 tez-title2 text-truncate">พร้อมให้แลกแล้ววันนี้</h6>
@@ -58,14 +58,14 @@
             </div>
           </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#post-carousel" data-bs-slide="prev">
+        <!-- <button class="carousel-control-prev" type="button" data-bs-target="#post-carousel" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#post-carousel" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
-        </button>
+        </button> -->
       </div>
     </body>
   </div>
@@ -76,10 +76,27 @@
 </script>
 
 <style scoped>
+
+.carousel-inner{
+   height: 580px;
+}
+.carousel-indicators [data-bs-target] {
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  background-color: #FB743E;
+  
+}
+.carousel-indicators {
+    bottom: 15%;
+    left: -35%;
+}
 .card{
   width: 35rem; 
   height: 25rem; 
   color: black;
+  left: -5%;
+  bottom: 3%;
 }
 .card-text{
 font-family: 'Poppins';
@@ -120,6 +137,8 @@ text-align: left;
 }
 .tez-btn {
   border: none;
+  width: 145px;
+  height: 48px;
 }
 
 .tez-mc {
