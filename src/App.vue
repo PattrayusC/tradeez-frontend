19e479a48fee135eb4afb634e0ce2d310e78dd07
Yutthasat Phoncharoenpong <pyutthasat@gmail.com>
2023-05-08 23:44:05 +0700

    app.vue add header & footnote

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,126 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div>
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom container">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <span class="LOGO1 fs-4 fw-bold">Trade </span>&nbsp
+        <span class="LOGO2 fs-4 fw-bold">EZ</span>&nbsp&nbsp
+        <span class="LOGO1 fs-4 fw-bold">ซื้อขาย </span>
+        <span class="LOGO2 fs-4 fw-bold">สบายใจ </span>
+      </a>
+      <ul class="nav">
+        <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Computer</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Food</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Lifestyle</a></li>
+        <li class="nav-item">
+          <a href="#" class="nav-login nav-link">LOGIN</a>
+        </li>
+      </ul>
+    </header>
+  </div>
+  <div class="min-vh-100 d-flex flex-column justify-content-between">
+    <div class="container">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <body class="d-flex flex-column" style="margin-bottom: 5%;">
+        <RouterView />
+      </body>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="Footer">
+      <footer class="mt-auto py-3 container">
+        <a href="/"
+          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none footerlogo">
+          <span class="LOGO3 fs-4 fw-bold">Trade </span>&nbsp
+          <span class="LOGO2 fs-4 fw-bold">EZ</span>&nbsp&nbsp
+          <span class="LOGO3 fs-4 fw-bold">ซื้อขาย </span>
+          <span class="LOGO2 fs-4 fw-bold">สบายใจ </span>
+        </a>
+        <p class="footer-text">เว็บไซต์เพื่อคนชอบช็อป ซื้อง่าย ขายคล่อง</p>
+        <br />
+        <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-0 border-top">
+          <p class="footer-text">
+            &copy; 2023 Trade EZ, EGCO 427 Mahidol University.
+          </p>
+        </div>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.LOGO1 {
+  font-family: "PT Serif";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 42px;
+  color: #272343;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.LOGO2 {
+  font-family: "PT Serif";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 42px;
+  color: #fb743e;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.LOGO3 {
+  font-family: "PT Serif";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 42px;
+  color: #ffffff;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.nav-link {
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #6d7280;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.nav-login {
+  font-family: "PT Serif";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 22px;
+  /* Accents */
+  color: #fb743e;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.Footer {
+  background-color: #272343;
 }
 
-nav a:first-of-type {
-  border: 0;
+.footer-text {
+  width: 336px;
+  height: 24px;
+  left: 300px;
+  top: 84px;
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  opacity: 0.6;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.footerlogo {
+  left: 14.27%;
+  right: 68.12%;
+  top: 11.78%;
+  bottom: 78.12%;
 }
 </style>
