de087e7ccefb404454f4e2d8711fb41d61519300
Yutthasat Phoncharoenpong <pyutthasat@gmail.com>
2023-05-09 21:15:12 +0700

    all post 90%

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
